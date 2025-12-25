a55095a7c88d6f95446e8a3e365a985cfaff3ebb
VEM-252 <np64574475@gmail.com>
2025-12-25 15:01:57 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -14,7 +14,7 @@ from pyrogram import idle
 import config
 from config import BANNED_USERS
 from VIPMUSIC import HELPABLE, LOGGER, app, userbot
-from VIPMUSIC.core.call import VIPBot
+from VIPMUSIC.core.call import VIP
 from VIPMUSIC.plugins import ALL_MODULES
 from VIPMUSIC.utils.database import get_banned_users, get_gbanned
 
